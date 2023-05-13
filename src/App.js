cbcb46c6da9488c81a0d3df1e75cac15cd766f92
andresRivera123 <88514682+andresRivera123@users.noreply.github.com>
2023-05-12 21:55:38 -0500

    app clima ciudad - v0.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/formulario/form';
+import WeatherApp from './components/maps/weatherApp';
 
 function App() {
 
-  return <Form/>
+  return <WeatherApp/>
 }
 
 export default App;
